74f5ea3ab58030849c5fe5495c159cfba11ec3e0
seth shoemaker <contact@sethshoemaker.com>
2025-04-18 10:52:01 -0400

    rename output file, add implementation

--- src/main/java/com/sethshoemaker/block_plugin/BlockPlugin.java
+++ src/main/java/com/sethshoemaker/block_plugin/BlockPlugin.java
@@ -1,10 +1,23 @@
 package com.sethshoemaker.block_plugin;
 
+import java.time.LocalTime;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("ClockPlugin is enabled!");
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            if(getServer().getOnlinePlayers().size() == 0){
+                return;
+            }
+            LocalTime now = LocalTime.now();
+            int minute = now.getMinute();
+            if(minute % 5 != 0){
+                return;
+            }
+            int hour = now.getHour();
+            getServer().broadcastMessage("It is currently " + hour + ":" + String.format("%02d", minute));
+        }, 0L, 20L * 60L); // 20 ticks per second × 60 seconds = 1 minute
     }
 }
